e5339a7c3a663814cbb308249b423a31ed685186
nikhilbadriprasad <67098637+nikhilbadriprasad@users.noreply.github.com>
2020-10-04 14:26:34 +0100

    The package contains three functions

---
+++ plot.climr_gp_fit.R
@@ -0,0 +1,50 @@
+#' Plot climr_gp_fit output
+#'
+#' @param x Output from the \code{\link{gp_fit}} function
+#' @param time_grid  An optional time grid over which to produce fitted values of the model
+#' @param ... Other arguments to plot (not currently implemented)
+#'
+#' @return Nothing: just a nice plot
+#' @seealso \code{\link{load_clim}}, \code{\link{gp_fit}}
+#' @export
+#' @import ggplot2
+#' @importFrom stats "sd" "optim"
+#' @importFrom tibble "tibble"
+#' @importFrom viridis "scale_color_viridis"
+#'
+#' @examples
+#' data1 = load_clim('NH')
+#' data2 = gp_fit(data1, 'BFGS')
+#' plot(data2)
+#' @export
+plot.climr_gp_fit <- function(x, time_grid = pretty(x$data$year, n = 100), ...) {
+
+  # Create global variables to avoid annoying CRAN notes
+  DJF = Dec = `J-D` = Jan = SON = Year = month = pred = quarter = temp = year = NULL
+
+  # Create a nice plot from the output of gp_fit
+
+  # Get the data set out
+  df <- x$data
+
+  # Get some predicted values based on the time_grid
+  if(x$method == 'Nelder-Mead') {
+    fits <- tibble(time_grid, x$model)
+  } else if(x$method == 'BFGS') {
+    fits <- tibble(time_grid, x$model)
+  } else if(x$method == 'SANN') {
+    fits <- tibble(time_grid, x$model)
+  } else if(x$method == 'Brent') {
+    fits <- tibble(time_grid, x$model)
+  }
+
+  # Finally create the plot
+  ggplot(df, aes(year, temp)) +
+    geom_point(aes(colour = temp)) +
+    theme_bw() +
+    xlab('Year') +
+    ylab('Temperature anomaly') +
+    geom_line(data = fits, aes(x = time_grid, y = x$model, colour = x$data$temp)) +
+    theme(legend.position = 'None') +
+    scale_color_viridis()
+}
